f08c188c6380cf3617a1fa52f5f0f990ca5d073e
Abdullah Numan <anewman15@hotmail.com>
2020-07-07 21:42:09 +0600

    Convert article_info_array to csv file

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 require_relative '../lib/article'
 
 
@@ -7,7 +8,12 @@ doc = Nokogiri::HTML(open("https://www.nytimes.com/topic/subject/space-and-cosmo
 
 article_list = doc.css('#stream-panel div ol li')
 
-article_list.each do |article|
+articles_csv = CSV.open("./article_list.csv", "a+")
+articles_csv << %w[Title Author Summary URL]
+
+article_list.each_with_index do |article, index|
   article = Article.new
-  puts article.article_info_arr(article_list)
+  puts article.article_info_arr(article_list[index])
+  puts ""
+  articles_csv << article.article_info_arr(article_list[index])
 end
--- lib/article.rb
+++ lib/article.rb
@@ -7,26 +7,26 @@ class Article
     
   end
 
-  def article_info_arr(doc)
-    [title(doc), author(doc), summary(doc), article_url(doc)]
+  def article_info_arr(article_list)
+    [title(article_list), author(article_list), summary(article_list), article_url(article_list)]
   end
 
   private
 
-  def title(doc)
-    doc.css('.e1xfvim30').text
+  def title(article_list)
+    article_list.css('.e1xfvim30').text
   end
 
-  def author(doc)
-    doc.css('.ea5icrr0 span').text
+  def author(article_list)
+    article_list.css('.ea5icrr0 span').text
   end
 
-  def summary(doc)
-    doc.css('.e1xfvim31').text
+  def summary(article_list)
+    article_list.css('.e1xfvim31').text
   end
 
-  def article_url(doc)
-    link = doc.css(".css-1l4spti a")
+  def article_url(article_list)
+    link = article_list.css(".css-1l4spti a")
     article_url = "https://www.nytimes.com/topic/subject/space-and-cosmos#{link.attr('href')}"
   end
 end
