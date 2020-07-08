e0efbf002e15335b6b26d349fae33f733f9f28c6
Abdullah Numan <anewman15@hotmail.com>
2020-07-08 23:41:22 +0600

    Add error handling in main.rb and fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'nokogiri'
 gem 'rspec'
+gem 'rubocop'
 # gem "rails"
--- bin/main.rb
+++ bin/main.rb
@@ -3,14 +3,23 @@ require 'open-uri'
 require 'csv'
 require_relative '../lib/article'
 
-doc = Nokogiri::HTML(open('https://www.nytimes.com/topic/subject/space-and-cosmos'))
-
+open_nyt_url = URI.open('https://www.nytimes.com/topic/subject/space-and-cosmos')
+doc = Nokogiri::HTML(open_nyt_url)
 article_list = doc.css('#stream-panel div ol li')
 
 articles_csv = CSV.open('./article_list.csv', 'a+')
 articles_csv << %w[Title Author Summary URL]
 
 article_list.each_with_index do |_article, index|
-  new_article = Article.new
-  articles_csv << new_article.article_info_arr(article_list[index])
+  begin
+    open_nyt_url
+  rescue StandardError => e
+    puts "Error: #{e}"
+  else
+    new_article = Article.new
+    articles_csv << new_article.article_info_arr(article_list[index])
+  ensure
+    puts 'Making sure data is being scrapped...'
+    sleep 1
+  end
 end
